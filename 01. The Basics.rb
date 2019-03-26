9e09f0eb75c7625edc679e2fd35e827cb82ab8b2
Claire Till <clairetill@users.noreply.github.com>
2019-03-25 18:16:10 -0700

    Add files via upload

---
+++ 01. The Basics.rb
@@ -0,0 +1,41 @@
+1. "Claire" + "Till"
+
+2. thousands = 4936 / 1000
+hundreds = 4936 % 1000 / 100
+tens = 4936 % 1000 % 100 / 10
+ones = 4936 % 1000 % 100 % 10
+
+3. movies = { harry_potter: 2005,
+           sorcerers_stone: 2006,
+           azkaban: 2007,
+           goblet_of_fire: 2008,
+           last_one: 2009 }
+
+puts movies[:harry_potter]
+puts movies[:sorcerers_stone]
+puts movies[:azkaban]
+puts movies[:goblet_of_fire]
+puts movies[:last_one]
+
+4. dates = [2005, 2006, 2007, 2008, 2009]
+puts dates[0]
+puts dates[1]
+puts dates[2]
+puts dates[3]
+puts dates[4]
+
+5. factorial_5 = 5 * 4 * 3 * 2 * 1
+factorial_6 = 6 * 5 * 4 * 3 * 2 * 1
+factorial_7 = 7 * 6 * 5 * 4 * 3 * 2 * 1
+factorial_8 = 8 * 7 * 6 * 5 * 4 * 3 * 2 * 1
+
+puts "factorial_5 #{factorial_5}"
+puts "factorial_6 #{factorial_6}"
+puts "factorial_7 #{factorial_7}"
+puts "factorial_8 #{factorial_8}"
+
+6. puts 2.1 * 2.1
+puts 6.7 * 6.7
+puts 12.4 * 12.4
+
+7. #This error message tells you that you used the wrong type of closing bracket. 
