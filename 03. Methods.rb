bfa6ef0c66442fa9bee12d33f56adbec604017a7
Claire Till <clairetill@users.noreply.github.com>
2019-03-27 11:24:34 -0700

    Add files via upload

---
+++ 03. Methods.rb
@@ -0,0 +1,35 @@
+1. def greeting(name)
+  "Hello, " + name + ". How are you doing?"
+end
+
+puts greeting("Bob")
+
+2. 1. x = 2    # => 2
+
+ puts x = 2    # nil
+
+ p name = "Joe"    # => "Joe"
+
+ four = "four"    # => "four"
+
+ print something = "nothing"    # => nil
+
+ 3. def multiply(number1, number2)
+  number1 * number2
+end
+
+puts multiply(0, 1)
+
+4. It will not print anything to the screen.
+
+5. 1. def scream(words)
+      words = words + "!!!!"
+      puts words
+    end
+
+    scream("Yippeee")
+
+    still returns nil
+
+6. You are calling a method called calculate_product that requires two arguments, but you are only providing one.
+
